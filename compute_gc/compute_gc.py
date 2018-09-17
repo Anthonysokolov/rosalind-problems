4728d129549c662647724d57f9e61fbb7734284e
Anthony <anthonysokolov20@gmail.com>
2018-09-17 09:43:59 -0400

    Added compute_gc

---
+++ compute_gc/compute_gc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Compute the GC content of a strand of DNA
+"""
+from rosalind import txt_to_list
+
+def compute_gc(string):
+    '''
+    Returns the ratio of GC nucleotides in a string of DNA
+    '''
+    var = list(string)
+    count = 0
+    for item in var:
+        if item == 'G':
+            count += 1
+        elif item == 'C':
+            count += 1
+    
+    return count/len(string)
+  
+
+print(txt_to_list('rosalind_gc.txt'))
