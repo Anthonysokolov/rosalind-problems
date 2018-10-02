3408049b71e2e999b4aed6183bec8378e1a99014
Anthony <anthonysokolov20@gmail.com>
2018-10-01 22:04:20 -0400

    Added rev_complement

---
+++ rev_complement/DNA_complement.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+http://rosalind.info/problems/revc/
+Returns the reverse complement of a strand of DNA
+"""
+import sys
+sys.path.append('..')
+from rosalind import format_txt, concatenate
+
+
+def rev_complement(strand):
+    '''
+    Returns the reverse complement of a strand of DNA
+    '''
+    complements = {'A':'T','T':'A','C':'G','G':'C'}
+    complement = ''
+    
+    for item in list(strand):
+        complement += complements[item]
+    
+    return reverse(complement)
+
+def reverse(strand):
+    '''
+    Returns a reverse ordered string
+    '''
+    return concatenate(list(strand)[::-1])
+
+file = 'rosalind_revc.txt'
+
+print(rev_complement(format_txt(file)))
